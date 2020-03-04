0d3da55e6c49aa8acf40759386adbc68882b0d50
Ethan Irvine <edog.irvine@gmail.com>
2020-03-03 18:05:01 -0700

    designed the wabpage

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 
@@ -14,9 +13,7 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(routes)
-
-
+app.use(routes);
 app.listen(PORT, function() {
   
   console.log("Server listening on: http://localhost:" + PORT);
